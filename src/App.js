3fd342f66414ee50b4ff9c83f0f562bf26ae7b52
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-12-11 02:27:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  
+  function handleLink(){
+    alert("Going to the page")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <a href="https://www.codespeedy.com/" onClick={handleLink}>Go to the page</a>
+      
     </div>
   );
 }
